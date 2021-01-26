5f5b107cde1def59f4446280b08349283a3114ea
czh <515740090@qq.com>
2021-01-26 19:41:12 +0800

    再次提交

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -176,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49939" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56888" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
